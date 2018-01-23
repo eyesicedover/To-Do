7d5b31b10b1e98d6501c2c4a08a79fde6adb96e3
Stephanie Faber <eyesicedover@gmail.com>
2018-01-22 20:29:09 -0800

    add html and js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
+function toDoItem(desc, date) {
+  this.describe = desc;
+  this.date = date;
+}
+
 $(document).ready(function() {
-  $("#todo-Form").submit(function(event) {
+  $("form#todo-Form").submit(function(event) {
     event.preventDefault();
+    var toDoTask = $("input#todo-Desc").val();
+    var toDoDate = $("input#todo-Date").val();
+    var newtoDoItem = new toDoItem(toDoTask, toDoDate);
+
+    $(".toDo").show();
+    $("ul#todo-List").append("<li class='thing'><span class='thisItem'> I need to " + newtoDoItem.describe + ", by " + newtoDoItem.date + "</span></li>");
+
+    $("li.thing").unbind().click(function() {
+      var thisDescribe = $(this).text();
+      $(".done").show();
+      $("ul#done-List").append("<li>" + thisDescribe + "</li>");
+      $(this).remove();
+    });
   });
 });
